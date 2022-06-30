af63a6c92245483741f660d82a79ec854527a2b6
Shayokh144 <shayokhbd144@gmail.com>
2022-06-30 14:38:24 +0700

    edit danger file remove force fail

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -12,6 +12,4 @@ if !changelogChanged && sourceChanges != nil {
 warn("DANGER IS ADDED")
 message("Highlight something in the table")
 warn("Something pretty bad, but not important enough to fail the build")
-fail("Something that must be changed")
-
 markdown("Free-form markdown that goes under the table, so you can do whatever.")
